7ec3afcdee496b51a580dbc674ad7009577abe1c
LobetzSerg <lobet1501@gmail.com>
2024-07-17 19:33:33 +1000

    13

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import '../styles/globals.css'
 import MainContainer from '../components/MainContainer/MainContainer';
+import Head from 'next/head';
 
 
 function MyApp({ Component }) {
 
 
   return (
-   
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <MainContainer >
-        <Component  />
+        <Component />
       </MainContainer>
-
+    </>
   )
 }
 
